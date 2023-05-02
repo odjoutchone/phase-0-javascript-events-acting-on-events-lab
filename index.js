e9c70ff9465541a3a657e408a79f0538f0f23c45
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-05-02 15:43:59 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger')
+    const currentLeft = parseInt(dodger.style.left)
+    dodger.style.left = `${currentLeft - 4}px`
+  }
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger')
+    const currentLeft = parseInt(dodger.style.left)
+    dodger.style.left = `${currentLeft + 4}px`
+  }
+  
